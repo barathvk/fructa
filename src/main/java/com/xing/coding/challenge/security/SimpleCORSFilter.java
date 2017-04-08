6e1f1dcf789ef22eeff6b2ce6f6447a7ce2eadf3
Barath Kumar <barathvk@gmail.com>
2017-04-08 04:40:44 -0500

    CORS priority

--- src/main/java/com/xing/coding/challenge/security/SimpleCORSFilter.java
+++ src/main/java/com/xing/coding/challenge/security/SimpleCORSFilter.java
@@ -8,35 +8,34 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 @Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class SimpleCORSFilter implements Filter {
-  private final Logger log = LoggerFactory.getLogger(SimpleCORSFilter.class);
-  @Override
-  public void init(FilterConfig filterConfig) throws ServletException {
-    log.info("SimpleCORSFilter init");
-  }
 
-  @Override
   public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
 
-    HttpServletRequest request = (HttpServletRequest) req;
     HttpServletResponse response = (HttpServletResponse) res;
-
-    response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
-    response.setHeader("Access-Control-Allow-Credentials", "true");
+    HttpServletRequest request = (HttpServletRequest) req;
+    response.setHeader("Access-Control-Allow-Origin", "*");
     response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
     response.setHeader("Access-Control-Max-Age", "3600");
-    response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
-
-    chain.doFilter(req, res);
+    response.setHeader("Access-Control-Allow-Credentials", "true");
+    response.setHeader("Access-Control-Allow-Headers", "Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization");
+    if(request.getMethod().equals(HttpMethod.OPTIONS.name())){
+      response.setStatus(HttpStatus.NO_CONTENT.value());
+    }else{
+      chain.doFilter(req, res);
+    }
   }
 
-  @Override
-  public void destroy() {
+  public void init(FilterConfig filterConfig) {}
 
-  }
+  public void destroy() {}
 }
