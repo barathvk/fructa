9c0fb1d176e87522447727d05085fff228e36d63
Barath Kumar <barathvk@gmail.com>
2017-04-08 00:39:26 -0500

    Disable host key checking

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh root@fructa.ga "cd /var/fructa; git pull origin master; docker-compose pull; docker-compose up -d"
+ssh -oStrictHostKeyChecking=no root@fructa.ga "cd /var/fructa; git pull origin master; docker-compose pull; docker-compose up -d"
